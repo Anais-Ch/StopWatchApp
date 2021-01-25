7fa0ce4144418eaecfd8f38370c7395d3567e6db
Anais-Ch <anais.chazeix@orange.fr>
2021-01-25 21:48:12 +0100

    Add buutons display behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StopWatchApp';
+  //
+  public isStarted = false;
+  public isPaused = false;
+
+  //
+
+  public start(): void {
+    this.isStarted =  true;
+  }
+
+  public stop(): void {
+    this.isStarted =  false;
+  }
+
+  public pause(): void {
+    this.isPaused =  true;
+  }
+
+  public continue(): void {
+    this.isPaused =  false;
+  }
+
 }
